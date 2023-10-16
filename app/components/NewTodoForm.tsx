956a62caf53ddfbc275a3aafda8456754dc93d01
victorkane <victorkane@gmail.com>
2023-10-16 06:52:30 -0300

    style: create new todo form styling via tailwind classes

--- app/components/NewTodoForm.tsx
+++ app/components/NewTodoForm.tsx
@@ -1,9 +1,18 @@
 const NewTodoForm = () => {
   return (
     <form action=''>
-      <h2>Create a new Todo</h2>
-      <input type='text' name='' id='' />
-      <button type='submit'>Add Todo</button>
+      <h2 className='mb-2 font-medium'>Create a New Todo</h2>
+      <input
+        type='text'
+        name='title'
+        className='rounded border border-slate-400 px-2 py-0.5'
+      />
+      <button
+        type='submit'
+        className='ml-2 rounded bg-slate-700 px-2 py-1 text-sm text-white disabled:bg-opacity-50'
+      >
+        Add Todo
+      </button>
     </form>
   )
 }
